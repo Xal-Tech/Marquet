9ede98a4a61be4a346780653b05a094755a04090
aricasas <arican@alumno.hayas.edu.mx>
2020-04-13 23:22:22 -0500

    arregle lo de header

--- website/header.php
+++ website/header.php
@@ -1,29 +1,23 @@
-<head>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="header">
-		<div class="dropdown" align="top">
-			<img src="images/barritas.png" height="30px" class="menu">
-			<div class="menu-dropdown">
-				<div class="barra-buscar">
-					<input type="text" placeholder="Buscar negocio">
-				</div>
-				<a href="#">Productos</a>
-				<a href="#">Vendedores</a>
-				<a href="#">Mapa</a>
-			</div>
-		</div>
-		<p class="titulo">
-			<a href="#">Marquet
-				<img src="images/logo-marquet.png" height="70px" align="top">
-			</a>
-		</p>
-		<p class="cuenta">
-			<a href="#">
-				<img src="images/cuenta.png" height="70px" align="top">
-			</a>
-		</p>
+<div class="header">
+    <div class="dropdown" align="top">
+        <img src="images/barritas.png" height="30px" class="menu">
+        <div class="menu-dropdown">
+            <div class="barra-buscar">
+                <input type="text" placeholder="Buscar negocio">
+            </div>
+            <a href="#">Productos</a>
+            <a href="#">Vendedores</a>
+            <a href="#">Mapa</a>
+        </div>
     </div>
-</body>
-</html>
+    <p class="titulo">
+        <a href="#">Marquet
+            <img src="images/logo-marquet.png" height="70px" align="top">
+        </a>
+    </p>
+    <p class="cuenta">
+        <a href="#">
+            <img src="images/cuenta.png" height="70px" align="top">
+        </a>
+    </p>
+</div>
--- website/home.php
+++ website/home.php
@@ -11,27 +11,17 @@ if (!isset($_SESSION['loggedin'])) {
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="css/home.css">
-  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 </head>
 <body>
-    <div class="header">
-		<p class="titulo">
-			<img src="images/Barritas.png" height="30px" align="top" id="barritas">
-			Marquet
-			<img src="images/logo-marquet.png" height="70px" align="top">
-		</p>
-		<p class="cuenta">
-			<img src="images/cuenta.png" height="70px" align="top">
-		</p>
-    </div>
-		<div  class="buttons">
+	<?php include 'header.php'; ?>
+	<div  class="buttons">
 		<input type="button" id="barra" value="Buscar producto">
-		
+
 		<input type="button" id="productos" value="Categorias de productos" >
-		</div>
+	</div>
 	
 </body>
 </html>
-
-
