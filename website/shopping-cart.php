ffb99090b1d7ada8dd1c5015e73a18a66dc17358
maurisba <mauriciobaa@alumno.hayas.edu.mx>
2020-04-15 14:43:12 -0500

    changes

---
+++ website/shopping-cart.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+// Si no esta logged in mandarlo a la pagina de login
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.html');
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+</body>
+</html>
