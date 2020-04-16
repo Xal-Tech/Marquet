e2e2b20dc456f84c47bc88bfb0f9dd6de319aa89
aricasas <arican@alumno.hayas.edu.mx>
2020-04-16 16:36:58 -0500

    cambie index.html a php

--- website/myaccount.php
+++ website/myaccount.php
@@ -3,7 +3,7 @@ session_start();
 
 // Si no esta logged in mandarlo a la pagina de login
 if (!isset($_SESSION['loggedin'])) {
-    header('Location: index.html');
+    header('Location: login/index.html');
     exit;
 }
 
