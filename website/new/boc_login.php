922de98f929743ea5287ea2ee68bbd957be4ecf8
Alen Lean <lennynlean@hotmail.com>
2020-04-21 16:50:57 -0500

    Demo de página de registro

---
+++ website/new/boc_login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<?php 
+		include 'inc/link.php';
+	 ?>
+</head>
+<body>
+	<?php 
+		include 'inc/navbar.php';
+	 ?>
+	<div class="jumbotron">
+		<div class="row align-items-center">
+			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
+			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
+		</div>
+	</div>
+	 <div class="container">
+	 	<div class="card">
+	 		<div class="card-title">
+	 			<center>
+	 			<strong>Crea una cuenta</strong>
+	 			<br>
+	 			Es rápido y sencillo
+	 			</center>
+	 		</div>
+	 	</div>
+	 </div>
+</body>
+</html>
